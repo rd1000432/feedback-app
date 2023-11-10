b44742557e395146adb40ab503a7d0edc5f26c0f
Ronan <dineen1@gmail.com>
2023-11-10 13:06:40 +0100

    basic routing -aboutpage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import {v4 as uuidv4} from 'uuid'
+import { v4 as uuidv4 } from 'uuid'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { useState } from 'react'
 import Header from './components/Header'
 import FeedbackList from './components/Feedbacklist'
 import FeedbackData from './data/FeedbackData'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
+import AboutIconLink from './components/AboutIconLink'
+import AboutPage from './pages/AboutPage'
 
 function App() {
 
@@ -22,16 +25,28 @@ function App() {
     }
 
     return (
-        <>
+        <Router>
         <Header text="Feedback UI"/>
-        <div className="container">
-            <FeedbackForm handleAdd={addFeedback} />
-            <FeedbackStats feedback={feedback}/>
-            <FeedbackList 
-            feedback={feedback}
-            handleDelete={deleteFeedback}/>
-        </div>
-        </>
+            <div className="container">
+            <Routes>
+                <Route exact path='/' element={
+                    <>
+                    <FeedbackForm handleAdd={addFeedback} />
+                    <FeedbackStats feedback={feedback}/>
+                    <FeedbackList 
+                    feedback={feedback}
+                    handleDelete={deleteFeedback}/>
+                    </>
+                }>
+                   
+                </Route>
+
+                <Route path='/about' element={<AboutPage />} />
+
+            </Routes>
+            <AboutIconLink />
+            </div>
+        </Router>
     )
 }
 
--- src/components/Feedbacklist.jsx
+++ src/components/Feedbacklist.jsx
@@ -1,3 +1,4 @@
+import {motion, AnimatePresence} from 'framer-motion'
 import PropTypes from 'prop-types'
 import FeedbackItem from "./Feedbackitem"
 
@@ -7,14 +8,33 @@ function FeedbackList({ feedback, handleDelete }) {
   }
   return (
     <div className="feedback-list">
+      <AnimatePresence>
       {feedback.map((item) => (
+        <motion.div 
+        key={item.id}
+        initial={{opacity: 0}}
+        animate={{opacity: 1}}
+        exit={{opacity: 0}}
+        >
         <FeedbackItem 
         key={item.id} 
         item={item} 
         handleDelete={handleDelete} />
+        </motion.div>
       ))}
+      </AnimatePresence>
     </div>
   )
+  // return (
+  //   <div className="feedback-list">
+  //     {feedback.map((item) => (
+  //       <FeedbackItem 
+  //       key={item.id} 
+  //       item={item} 
+  //       handleDelete={handleDelete} />
+  //     ))}
+  //   </div>
+  // )
 }
 
 FeedbackList.propTypes = {
